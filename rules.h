0e53c22ff8dc8021bfe0386c5c59699f97d7b276
Ganesh Tiwari <gt20722@gmail.com>
2022-09-11 10:36:17 +0545

    First commit

---
+++ rules.h
@@ -0,0 +1,9 @@
+void rules()
+{
+
+	printf("\t\t\t 5 marks is given for each question\n");
+	printf("\t\t\tNo marks is given for wrong answer\n");
+	printf("\t\t\tThere is no negative marking for incorrect answer\n");
+	printf("\t\t\tUnauthorized persons are not allowed to login to admin section\n ");
+	
+}
